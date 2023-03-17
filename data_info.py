85ca2a9241178fcbd40a408a8d380f6e83f9203f
yuxuan.wu <yuxuan.wu17@gmail.com>
2023-03-17 12:48:34 -0400

    Initial commit

---
+++ data_info.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+# df = pd.read_csv("data/featureSelectedAllDataWithY.csv")
+# df = pd.read_csv("data/Cleaned_dat_encoded.csv")
+df = pd.read_csv("data/Cleaned_dat.csv")
+
+# Display the shape of the data frame
+print('Shape of data frame:', df.shape)
+
+print(df.columns)
+
+# Display the first few rows of the data frame
+print('\nFirst few rows of data frame:')
+print(df.head())
